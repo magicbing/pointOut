b577ad2e12755634f128839fdb29717c6709c20d
magicbing <932201328@qq.com>
2017-08-03 00:34:08 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # pointOut
 pointOut.js
-//1、引用pointOut.js	 <script src="js/pointOut.js"></script>
-//2、pointOut(showText, dTop) 
+//1、引用pointOut.js   <script src="js/pointOut.js"></script>
+//pointOutText('showText', dTop)
 //3、第1个参数showText是要提示的文字，宽度可以自适应
 //4、第2个参数dTop是距离顶部的百分比，可以不填默认是50%
 //5、在屏幕中间显示的提示框，持续1.7秒
+//基于jQuery
--- pointOut.js
+++ pointOut.js
@@ -1,15 +1,19 @@
-//1、引用pointOut.js	 <script src="js/pointOut.js"></script>
-//2、pointOut(showText, dTop) 
+//1、引用pointOut.js   <script src="js/pointOut.js"></script>
+//pointOutText('showText', dTop)
 //3、第1个参数showText是要提示的文字，宽度可以自适应
 //4、第2个参数dTop是距离顶部的百分比，可以不填默认是50%
 //5、在屏幕中间显示的提示框，持续1.7秒
+//基于jQuery
 
-function pointOutText(showText, dTop){
-	if (typeof(dTop) == "undefined"){
-			dTop = 50;
-	}
-	var pointOutText = showText;
-	var distanceTop = dTop;
-	$('body>div').first().append('<div id="pointDIV" style="display: none;"></div>');
-    $('#pointDIV').attr('style', 'position: fixed;top: ' + dTop + '%;left: 50%;-webkit-transform: translate(-50%, -50%);transform:translate(-50%, -50%);background: rgba(113, 111, 111, 0.82);color: white;border-radius: 5px;padding: 5px;text-align: center;').stop().text(pointOutText).show().delay(1700).hide(0);
-}
+(function(){
+  pointOutText =  function(showText, dTop){
+    if (typeof(dTop) == "undefined"){
+        dTop = 50;
+    }
+    var pointOutText = showText;
+    var distanceTop = dTop;
+    $('body>div').first().append('<div id="pointDIV" style="display: none;"></div>');
+      $('#pointDIV').attr('style', 'position: fixed;top: ' + dTop + '%;left: 50%;-webkit-transform: translate(-50%, -50%);transform:translate(-50%, -50%);background: rgba(113, 111, 111, 0.82);color: white;border-radius: 5px;padding: 5px;text-align: center;').stop().text(pointOutText).show().delay(1700).hide(0);
+  }
+  return pointOutText
+}())
